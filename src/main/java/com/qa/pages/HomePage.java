c2ea94e3dbe4e8c038340bdf77354f36270de1ba
sumitsaklani <sumitsaklani93@gmail.com>
2018-12-16 19:20:05 +0530

    yolo

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -7,38 +7,36 @@ import org.openqa.selenium.support.PageFactory;
 import com.qa.base.TestBase;
 
 public class HomePage extends TestBase{
-	
+
 	@FindBy(xpath="//td[contains(text(),'User: Sumit Saklani')]")
 	WebElement userNameLabel;
-	
+
 	@FindBy(xpath="//a[contains(text(),'Contacts')]")
 	WebElement contactLink;
-		
+
 	@FindBy(xpath="//a[contains(text(),'Deals')]")
 	WebElement dealsLink;
-	
+
 	@FindBy(xpath="//a[contains(text(),'Tasks')]")
 	WebElement tasksLink;
-	
+
 	public HomePage() {
 		PageFactory.initElements(driver,this);
 	}
-	
+
 	public boolean verifyUserName() {
 		return userNameLabel.isDisplayed();
 	}
-	
-	
-	
+
 	public String verifyHomePageTitle() {
 		return driver.getTitle();
 	}
-	
+
 	public ContactsPage clickOnContactsLink() {
 		contactLink.click();
 		return new ContactsPage();
 	}
-	
+
 	public DealsPage clickOnDealsLink() {
 		dealsLink.click();
 		return new DealsPage();
